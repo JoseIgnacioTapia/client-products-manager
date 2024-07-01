23af4dfbd5a0fdc68346b6ad45966ccbc68f2d5d
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-01 16:49:47 -0300

    Redireccionar con useNavigate

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import type { Router } from "@remix-run/router"; // Import the Router type after install it
 import Layout from "./layouts/Layout";
-import Products from "./views/Products";
+import Products, { loader as productsLoader } from "./views/Products";
 import NewProduct, { action as newProductAction } from "./views/NewProduct";
+import EditProduct from "./views/EditProduct";
 
 export const router: Router = createBrowserRouter([
   {
@@ -12,11 +13,16 @@ export const router: Router = createBrowserRouter([
       {
         index: true,
         element: <Products />,
+        loader: productsLoader,
       },
       {
         path: "/productos/nuevo",
         element: <NewProduct />,
         action: newProductAction,
+      },
+      {
+        path: "productos/:id/editar",
+        element: <EditProduct />,
       },
     ],
   },
